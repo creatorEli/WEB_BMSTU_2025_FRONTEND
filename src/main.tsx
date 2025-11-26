95562d71d4831d3f70e712e487d5cbc9c8a7d5d3
Ilya <creatoreli8@gmail.com>
2025-11-26 17:27:09 +0300

    added l6 frontend app

---
+++ src/main.tsx
@@ -0,0 +1,56 @@
+import './index.css'
+import "./resources/styles/style.css"
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+// import StartPage from './names'
+import ArmiesPage from './pages/Armies/armies'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ArmyPage from './pages/Onearmy/onearmy'
+import MainPage from './pages/Mainpage/mainpage'
+import { Provider } from 'react-redux'
+import { store } from './store'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainPage />
+  },
+  {
+    path: '/armies',
+    element: <ArmiesPage />
+  },
+  {
+    path: '/army/:id',
+    element: <ArmyPage />
+  }
+])
+console.log("worked")
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    {/* <ul>
+      <li>
+        <a href="/">Главная страница</a>
+      </li>
+      <li>
+        <a href="/new">Хочу на страницу с чем-то новеньким</a>
+      </li>
+      <li>
+        <a href="/names">Страница с именами</a>
+      </li>
+      <li>
+        <a href="/armies">Список армий</a>
+      </li>
+    </ul>
+    <hr /> */}
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>,
+)
+console.log("not worked")
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
