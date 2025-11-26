cfc7d2045bf4e65ecde5c59f11f66081dbf213dc
Ilya <creatoreli8@gmail.com>
2025-11-26 17:34:37 +0300

    added frontend for pages

---
+++ src/main.tsx
@@ -0,0 +1,42 @@
+import './index.css'
+import "./resources/styles/style.css"
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+// import StartPage from './names'
+import ArmiesPage from './pages/Armies/armies'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ArmyPage from './pages/Onearmy/onearmy'
+import MainPage from './pages/Mainpage/mainpage'
+import { Provider } from 'react-redux'
+import { store } from './store'
+// import { registerSW } from "virtual:pwa-register";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainPage />
+  },
+  {
+    path: '/armies',
+    element: <ArmiesPage />
+  },
+  {
+    path: '/army/:id',
+    element: <ArmyPage />
+  }
+], {
+  basename: import.meta.env.BASE_URL  // для гитхаба
+})
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>,
+)
+
+// if ("serviceWorker" in navigator) {
+//   registerSW()
+// }
---
+++ vite.config.ts
@@ -0,0 +1,63 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import mkcert from 'vite-plugin-mkcert'
+import fs from 'fs';
+import path from 'path';
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vite.dev/config/
+export default defineConfig({
+  //server: { port: 3000 },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8084",
+        changeOrigin: true,
+        secure: false,
+        //rewrite: (path) => path.replace(/^\/api/, "/"),
+      },
+    },
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'cert.key')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'cert.crt')),
+    },
+    port: 3001
+  },
+  //plugins: [react(), mkcert(),],
+  plugins: [
+    react(),
+    mkcert(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "Times amd Armies - github pages + pwa + sw",
+        short_name: "Times And Armies",
+        scope: '/times_and_armies_front/',
+        start_url: '/times_and_armies_front/',
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#B52600",
+        orientation: "portrait-primary",
+        icons: [
+          {
+            "src": "/logo192.png",
+            "type": "image/png", "sizes": "192x192"
+          },
+          {
+            "src": "/logo512.png",
+            "type": "image/png", "sizes": "512x512"
+          }
+        ],
+      }
+    })],
+  base: '/times_and_armies_front/',
+  // compilerOptions: {
+  //   "types": [
+  //     "vite-plugin-pwa/info.d.ts",
+  //     "vite-plugin-pwa/client.d.ts"
+  //   ],
+  // }
+})
